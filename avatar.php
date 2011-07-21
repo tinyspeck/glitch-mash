ea56c9a03a87290b401b83b2f232a783d14206d2
Cal Henderson <cal@iamcal.com>
2011-07-21 11:08:20 -0700

    show 5 most recent battles no individual avatar page. slight layout change

--- avatar.php
+++ avatar.php
@@ -50,6 +50,44 @@
 
 
 	#
+	# fetch the most recent 5 battles
+	#
+
+	$recent_count = 5;
+
+	$ret1 = db_fetch("SELECT * FROM glitchmash_votes WHERE win_id=$id_enc ORDER BY date_updated DESC LIMIT $recent_count");
+	$ret2 = db_fetch("SELECT * FROM glitchmash_votes WHERE lose_id=$id_enc ORDER BY date_updated DESC LIMIT $recent_count");
+
+	$votes = array();
+	$smarty->assign_by_ref('votes', $votes);
+
+	foreach (array_merge($ret1['rows'], $ret2['rows']) as $row){
+
+		$opp_id = $row['win_id'] == $avatar['id'] ? $row['lose_id'] : $row['win_id'];
+		$opp = db_single(db_fetch("SELECT * FROM glitchmash_avatars WHERE id='$opp_id'"));
+		$opp_tsid_enc = AddSlashes($opp['player_tsid']);
+		$opp['player'] = db_single(db_fetch("SELECT * FROM glitchmash_players WHERE tsid='$opp_tsid_enc'"));
+
+		$opp['url_50'] = str_replace('_172.png', '_50.png', $opp['url']);
+
+		$votes[] = array(
+			'did_win'	=> !!($row['win_id'] == $avatar['id']),
+			'avatar'	=> $opp,
+			'date_updated'	=> $row['date_updated'],
+		);
+	}
+
+	usort($votes, 'local_vote_sort');
+
+	function local_vote_sort($a, $b){
+		return $b['date_updated'] - $a['date_updated'];
+	}
+
+	$votes = array_slice($votes, 0, $recent_count);
+
+
+
+	#
 	# output
 	#
 
